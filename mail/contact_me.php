eb55c416e913aad20497440180e3527ade0cddf0
laurenslyman <laurenslyman7@yahoo.com>
2017-01-05 00:06:48 -0500

    contact form

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,211 +1,26 @@
-
-<?PHP 
-require('./PHPMailer/class.phpmailer.php');
-$mail=new PHPMailer();
-$mail->CharSet = 'UTF-8';
-
-$body = 'This is the message';
-
-$mail->IsSMTP();
-$mail->Host       = 'smtp.gmail.com';
-
-$mail->SMTPSecure = 'tls';
-$mail->Port       = 587;
-$mail->SMTPDebug  = 1;
-$mail->SMTPAuth   = true;
-
-$mail->Username   = 'me.sender@gmail.com';
-$mail->Password   = '123!@#';
-
-$mail->SetFrom('me.sender@gmail.com', $name);
-$mail->AddReplyTo('no-reply@mycomp.com','no-reply');
-$mail->Subject    = 'subject';
-$mail->MsgHTML($body);
-
-$mail->AddAddress('abc1@gmail.com', 'title1');
-$mail->AddAddress('abc2@gmail.com', 'title2'); /* ... */
-
-$mail->AddAttachment($fileName);
-$mail->send();
-
-
-
-
-
-
-
-
-
-/*$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$human = intval($_POST['human']);
-
-
-$from = 'Demo Contact Form'; 
-$to = 'example@domain.com'; 
-$subject = 'Message from Contact Demo ';
-$body = "From: $name\n E-Mail: $email\n Message:\n $message";
-
-if (!$_POST['name']) {
-	$errName = 'Please enter your name';
-if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-	$errEmail = 'Please enter a valid email address';
-}if ($human !== 5) {
-	$errHuman = 'Your anti-spam is incorrect';
-}
-    
-    <?php echo "<p class='text-danger'>$errName</p>";?>
-value="<?php echo htmlspecialchars($_POST['name']); ?>">
-
 <?php
-	if (isset($_POST["submit"])) {
-		$name = $_POST['name'];
-		$email = $_POST['email'];
-		$message = $_POST['message'];
-		$human = intval($_POST['human']);
-		$from = 'Demo Contact Form'; 
-		$to = 'example@domain.com'; 
-		$subject = 'Message from Contact Demo ';
-		
-		$body = "From: $name\n E-Mail: $email\n Message:\n $message";
-
-// Check if name has been entered
-if (!$_POST['name']) {
-	$errName = 'Please enter your name';
-}
- 
-// Check if email has been entered and is valid
-if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-	$errEmail = 'Please enter a valid email address';
-}
- 
-//Check if message has been entered
-if (!$_POST['message']) {
-	$errMessage = 'Please enter your message';
-}
-//Check if simple anti-bot test is correct
-if ($human !== 5) {
-	$errHuman = 'Your anti-spam is incorrect';
-}
-        
-
-// If there are no errors, send the email
-if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
-	if (mail ($to, $subject, $body, $from)) {
-		$result='<div class="alert alert-success">Thank You! We will be in touch.</div>';
-	} else {
-		$result='<div class="alert alert-danger">Sorry, there was an error sending your message. Please try again later.</div>';
-	}
-}
-        
-        <?php echo $result; ?>
-
-<?php
-	if (isset($_POST["submit"])) {
-		$name = $_POST['name'];
-		$email = $_POST['email'];
-		$message = $_POST['message'];
-		$human = intval($_POST['human']);
-		$from = 'Demo Contact Form'; 
-		$to = 'cafecoastal@gmail.com'; 
-		$subject = 'Message from Contact Demo ';
-		
-		$body = "From: $name\n E-Mail: $email\n Message:\n $message";
- 
-		// Check if name has been entered
-		if (!$_POST['name']) {
-			$errName = 'Please enter your name';
-		}
-		
-		// Check if email has been entered and is valid
-		if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-			$errEmail = 'Please enter a valid email address';
-		}
-		
-		//Check if message has been entered
-		if (!$_POST['message']) {
-			$errMessage = 'Please enter your message';
-		}
-		//Check if simple anti-bot test is correct
-		if ($human !== 5) {
-			$errHuman = 'Your anti-spam is incorrect';
-		}
- 
-// If there are no errors, send the email
-if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
-	if (mail ($to, $subject, $body, $from)) {
-		$result='<div class="alert alert-success">Thank You! I will be in touch</div>';
-	} else {
-		$result='<div class="alert alert-danger">Sorry there was an error sending your message.   Please try again later</div>';
-	   }
-    }
-}
-?>
-
-<form class="form-horizontal" role="form" method="post" action="index.php">
-	<div class="form-group">
-		<label for="name" class="col-sm-2 control-label">Name</label>
-		<div class="col-sm-10">
-			<input type="text" class="form-control" id="name" name="name" placeholder="First & Last Name" value="<?php echo htmlspecialchars($_POST['name']); ?>">
-			<?php echo "<p class='text-danger'>$errName</p>";?>
-		</div>
-	</div>
-	<div class="form-group">
-		<label for="email" class="col-sm-2 control-label">Email</label>
-		<div class="col-sm-10">
-			<input type="email" class="form-control" id="email" name="email" placeholder="example@domain.com" value="<?php echo htmlspecialchars($_POST['email']); ?>">
-			<?php echo "<p class='text-danger'>$errEmail</p>";?>
-		</div>
-	</div>
-	<div class="form-group">
-		<label for="message" class="col-sm-2 control-label">Message</label>
-		<div class="col-sm-10">
-			<textarea class="form-control" rows="4" name="message"><?php echo htmlspecialchars($_POST['message']);?></textarea>
-			<?php echo "<p class='text-danger'>$errMessage</p>";?>
-		</div>
-	</div>
-	<div class="form-group">
-		<label for="tel" class="col-sm-2 control-label">Phone Number</label>
-		<div class="col-sm-10">
-			<input type="text" class="form-control" id="phone" name="number" placeholder="Your Answer">
-			<?php echo "<p class='text-danger'>$errHuman</p>";?>
-		</div>
-	</div>
-	<div class="form-group">
-		<div class="col-sm-10 col-sm-offset-2">
-			<input id="submit" name="submit" type="submit" value="Send" class="btn btn-primary">
-		</div>
-	</div>
-	<div class="form-group">
-		<div class="col-sm-10 col-sm-offset-2">
-			<?php echo $result; ?>	
-		</div>
-	</div>
-</form> */
-<?php
-/*// Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
+// Check for empty fields
+if(empty($_POST['name'])      ||
+   empty($_POST['email'])     ||
+   empty($_POST['phone'])     ||
+   empty($_POST['message'])   ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
-	echo "No arguments Provided!";
-	return false;
+   echo "No arguments Provided!";
+   return false;
    }
-	
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
-	
+   
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+   
 // Create the email and send the message
 $to = 'cafecoastal@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
+$headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
-return true;			
-?>*/
+return true;         
+?>
