21b44cc162def1fdaa27cd42e645f4b6c2942cf2
Ubuntu <ubuntu@ip-172-31-20-148.ec2.internal>
2017-01-08 23:06:07 +0000

    Fixed mailer

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,5 +1,4 @@
 <?php
-// Check for empty fields
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
    empty($_POST['phone'])     ||
@@ -10,40 +9,32 @@ if(empty($_POST['name'])      ||
    return false;
    }
    
+
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
-   
-require_once 'lib/swift_required.php';
+
+require_once '/home/ubuntu/cafecoastalcatering.github.io/vendor/swiftmailer/swiftmailer/lib/swift_required.php';
 
 // Create the Transport
-$transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
+$transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 587, "tls")
   ->setUsername(getenv('GMAIL_USER'))
   ->setPassword(getenv('GMAIL_PASS'))
   ;
 
-/*
-You could alternatively use a different transport such as Sendmail or Mail:
-
-// Sendmail
-$transport = Swift_SendmailTransport::newInstance('/usr/sbin/sendmail -bs');
-
-// Mail
-$transport = Swift_MailTransport::newInstance();
-*/
 
 // Create the Mailer using your created Transport
 $mailer = Swift_Mailer::newInstance($transport);
 
 // Create a message
-$message = Swift_Message::newInstance('Catering Inquiry')
-  ->setFrom(array('johndoe@example.com' => 'John Doe'))
+$swift_message = Swift_Message::newInstance('Catering Inquiry')
+  ->setFrom(array($email_address => $name))
   ->setTo(array('cafecoastal@gmail.com' => 'Cafe Coastal Catering'))
-  ->setBody('Here is the message itself')
+  ->setBody("Name: " . $name . "\nEmail: " . $email_address . "\nPhone number: " . $phone . "\n" . $message)
   ;
 
 // Send the message
-$result = $mailer->send($message);
+$result = $mailer->send($swift_message);
 return true;         
 ?>
